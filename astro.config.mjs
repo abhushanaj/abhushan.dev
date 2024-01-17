6c42c7bbc783447b4c8786385ebd5621272ecda5
Abhushan A. Joshi <abhushanadhikarijoshi@gmail.com>
2024-01-17 09:49:13 +0530

    feat: add react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-	server: { port: 3000 },
-	site: 'https://abhushan.dev'
+	server: {
+		port: 3000
+	},
+	site: 'https://abhushan.dev',
+	integrations: [react()]
 });
