cc352f35cb6f28a5c6c242ddfec304dbb0423655
Abhushan A. Joshi <abhushanadhikarijoshi@gmail.com>
2024-01-18 13:53:43 +0530

    tooling: setup eslint, prettier and react essentials

---
+++ .eslintrc.cjs
@@ -0,0 +1,50 @@
+const { getTokenSourceMapRange } = require('typescript');
+
+/** @type {import('eslint').Linter.Config} */
+const config = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended-type-checked',
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+		'prettier',
+		'plugin:react/jsx-runtime'
+	],
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 'latest',
+		ecmaFeatures: {
+			jsx: true
+		},
+		project: true,
+		tsconfigRootDir: __dirname
+	},
+	plugins: ['@typescript-eslint', 'react'],
+
+	env: {
+		browser: true,
+		node: true
+	},
+	rules: {
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': [
+			'error',
+			{
+				argsIgnorePattern: '^_',
+				varsIgnorePattern: '^_',
+				caughtErrorsIgnorePattern: '^_'
+			}
+		],
+		'no-shadow': 'off',
+		'@typescript-eslint/no-shadow': ['error'],
+		'no-console': [2, { allow: ['warn', 'error'] }]
+	},
+	settings: {
+		react: {
+			version: 'detect'
+		}
+	}
+};
+module.exports = config;
