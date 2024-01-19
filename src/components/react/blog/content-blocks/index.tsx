9d99ab904104579fa976b068264d82a285b17a2b
Abhushan A. Joshi <49617450+abhu-A-J@users.noreply.github.com>
2024-01-19 12:51:41 +0530

    Feat/custom mdx components (#23)
    * fix: correction on main height calculation (#19)
    
    * feat: add paragraph content block
    
    * feat: adding bold to content block
    
    * feat: links
    
    * feat: add block qoutes
    
    * feat: add heading blocks
    
    * feat: inline code block
    
    * feat: add list content blocks
    
    * fix: format

---
+++ src/components/react/blog/content-blocks/index.tsx
@@ -0,0 +1,32 @@
+import type { ComponentProps } from 'react';
+
+import { ExternalLink } from '@/components/react/link/external';
+import { InternalLink } from '@/components/react/link/internal';
+
+import { BlockQuote, BlockQuoteError } from './block-quote';
+import { Heading } from './heading';
+import { InlineCode } from './inline-code';
+import { List } from './list';
+import { Paragraph } from './paragraph';
+import { Strong } from './strong';
+
+type PolymorphicHeadingProps = Omit<ComponentProps<typeof Heading>, 'as'>;
+type PolymorphicListProps = Omit<ComponentProps<typeof List>, 'as'>;
+
+export const components = {
+	h2: (props: PolymorphicHeadingProps) => <Heading as="h2" {...props} />,
+	h3: (props: PolymorphicHeadingProps) => <Heading as="h3" {...props} />,
+	h4: (props: PolymorphicHeadingProps) => <Heading as="h4" {...props} />,
+	h5: (props: PolymorphicHeadingProps) => <Heading as="h5" {...props} />,
+	p: Paragraph,
+	b: Strong,
+	strong: Strong,
+	blockquote: BlockQuoteError,
+	BlockQuote,
+	a: InternalLink,
+	InternalLink,
+	ExternalLink,
+	InlineCode,
+	ul: (props: PolymorphicListProps) => <List as="ul" {...props} />,
+	ol: (props: PolymorphicListProps) => <List as="ol" {...props} />
+};
