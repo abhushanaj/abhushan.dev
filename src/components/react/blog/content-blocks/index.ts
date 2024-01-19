04a255a70bb699ae4f56eb1c58ba8abfd5c76679
Abhushan A. Joshi <abhushanadhikarijoshi@gmail.com>
2024-01-19 11:58:33 +0530

    feat: links

--- src/components/react/blog/content-blocks/index.ts
+++ src/components/react/blog/content-blocks/index.ts
@@ -1,8 +1,14 @@
+import { ExternalLink } from '@/components/react/link/external';
+import { InternalLink } from '@/components/react/link/internal';
+
 import { Paragraph } from './paragraph';
 import { Strong } from './strong';
 
 export const components = {
 	p: Paragraph,
 	b: Strong,
-	strong: Strong
+	strong: Strong,
+	a: InternalLink,
+	InternalLink,
+	ExternalLink
 };
