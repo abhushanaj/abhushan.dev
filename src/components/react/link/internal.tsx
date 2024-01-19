12aab1561b481b5648d38b624286cde55f37493d
Abhushan A. Joshi <abhushanadhikarijoshi@gmail.com>
2024-01-19 18:34:33 +0530

    feat: make lowContrast and highContrast links

--- src/components/react/link/internal.tsx
+++ src/components/react/link/internal.tsx
@@ -1,19 +1,31 @@
 import type { ComponentPropsWithoutRef } from 'react';
 
+type LinkVariants = 'lowContrast' | 'highContrast';
+
 type Props = ComponentPropsWithoutRef<'a'> & {
 	title: string;
+	variant?: LinkVariants;
 };
 
-export function InternalLink({ href, children, title, className, ...otherProps }: Props) {
+const linkVariantClassMap: Record<LinkVariants, string> = {
+	lowContrast:
+		'text-text-neutral-lc hover:text-text-neutral-hc underline underline-offset-4 decoration-ui-seperator-neutral-lc',
+	highContrast:
+		'text-text-neutral-hc underline underline-offset-4 decoration-ui-seperator-neutral-lc hover:decoration-ui-seperator-neutral-hc'
+};
+
+export function InternalLink({ href, children, title, variant = 'lowContrast', className = '', ...otherProps }: Props) {
 	if (!title || !title.trim().length) {
 		throw new Error('Add a title attribute on the <InternalLink title="" />');
 	}
+
+	const variantClassNames = linkVariantClassMap[variant];
 
 	return (
 		<a
 			href={href}
 			title={title}
-			className={`text-inherit inline-flex items-center gap-1 rounded-sm underline decoration-ui-seperator-neutral-lc underline-offset-4 hover:text-text-neutral-hc focus-visible:ring-2 focus-visible:ring-ui-neutral-focus-ring focus-visible:ring-offset-2 ${className}`}
+			className={`text-inherit inline-flex items-center gap-1 focus-visible:ring-2 focus-visible:ring-ui-neutral-focus-ring focus-visible:ring-offset-2 ${variantClassNames} ${className}`}
 			{...otherProps}
 		>
 			{children}
