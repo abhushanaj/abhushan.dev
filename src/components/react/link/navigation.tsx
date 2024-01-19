007739a1b9ae90198bc9391af43bcf8805d92146
Abhushan A. Joshi <49617450+abhu-A-J@users.noreply.github.com>
2024-01-19 18:59:21 +0530

    Chore/unify all links (#25)
    * fix: correction on main height calculation (#19)
    
    * feat: make lowContrast and highContrast links
    
    * feat: create a uniform nav link with active state

---
+++ src/components/react/link/navigation.tsx
@@ -0,0 +1,26 @@
+import type { ComponentPropsWithoutRef } from 'react';
+
+type Props = ComponentPropsWithoutRef<'a'> & {
+	title: string;
+	active?: boolean;
+};
+
+export function NavLink({ title, children, href, active = false, ...otherProps }: Props) {
+	if (!title || !title.trim().length) {
+		throw new Error('Add a title attribute on the <InternalLink title="" />');
+	}
+
+	const activeClassName = 'text-text-neutral-hc underline decoration-ui-seperator-neutral-hc';
+	const inActiveClassName = 'text-text-neutral-lc decoration-ui-seperator-neutral-lc ';
+
+	return (
+		<a
+			href={href}
+			title={title}
+			className={`underline-offset-4 hover:underline focus-visible:ring-2 focus-visible:ring-ui-neutral-focus-ring focus-visible:ring-offset-2 ${active ? activeClassName : inActiveClassName} `}
+			{...otherProps}
+		>
+			{children}
+		</a>
+	);
+}
