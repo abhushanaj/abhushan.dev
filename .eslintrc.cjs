d38ce0887d94c4a8978f28c91cf08adec68e89d8
Abhushan A. Joshi <49617450+abhu-A-J@users.noreply.github.com>
2024-01-21 17:58:47 +0530

    Feat/toc blog details (#29)
    * feat: make toc sticky on the top
    
    * feat: toc for blog details page
    
    * fix: add import plugin and remove unused import (#28)
    
    * feat: update eslint rule
    
    * feat: add toc blog details, update eslint lint and husky hooks
    
    * fix: typo in npm script

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,3 @@
-const { getTokenSourceMapRange } = require('typescript');
-
 /** @type {import('eslint').Linter.Config} */
 const config = {
 	root: true,
@@ -13,8 +11,8 @@ const config = {
 		'plugin:@typescript-eslint/recommended-type-checked',
 		'plugin:react/recommended',
 		'plugin:react-hooks/recommended',
-		'plugin:react/jsx-runtime',
 		'plugin:jsx-a11y/strict',
+		'plugin:react/jsx-runtime',
 		'prettier'
 	],
 	parserOptions: {
