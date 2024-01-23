b52d104f17a483e6eea14a710a3a81c1d6133c1d
Abhushan A. Joshi <49617450+abhu-A-J@users.noreply.github.com>
2024-01-23 10:42:01 +0530

    blog: get by type components (#30)
    * feat: start blog javascript for interviews
    
    * feat: show listing by types
    
    * feat: add listing components to flter series and standalone blogs

--- src/schemas/blog.ts
+++ src/schemas/blog.ts
@@ -14,5 +14,6 @@ export const blogSchema = z.object({
 	seoMetaData: seoMetaDataSchema,
 	title: z.string(),
 	publishedDate: z.date(),
-	inProgress: z.boolean().optional().default(false)
+	inProgress: z.boolean().optional().default(false),
+	type: z.enum(['series', 'standalone']).default('standalone')
 });
