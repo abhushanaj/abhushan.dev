d6640bb99885fe562700410f30756859e1bef920
Abhushan A. Joshi <abhushanadhikarijoshi@gmail.com>
2024-02-11 11:46:41 +0530

    fix: prevent dev server from breaking on new restarts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
