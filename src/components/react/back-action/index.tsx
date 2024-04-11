f5c992c70f5c88d2e23df04ad245a9a990f87441
Abhushan Adhikari Joshi <49617450+abhushanaj@users.noreply.github.com>
2024-04-11 13:49:20 +0530

    feat: addition of projects section on the home page (#170)
    * feat: addition of projects section on the home page
    
    * chore: remove unwanted content

--- src/components/react/back-action/index.tsx
+++ src/components/react/back-action/index.tsx
@@ -6,7 +6,7 @@ import { InternalLink } from '@/components/react/link/internal';
 
 type Props = ComponentProps<typeof InternalLink>;
 
-export function BlogBackAction({ href, title, children, ...otherPropps }: Props) {
+export function BackAction({ href, title, children, ...otherPropps }: Props) {
 	return (
 		<InternalLink href={href} title={title} {...otherPropps}>
 			<ArrowLeft className="h-[1em] w-[1em]" />
